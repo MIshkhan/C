ebf425916a75525b208d77ef7c3779356ce3816c
ishkhan <martirosyan.1996@list.ru>
2017-05-09 19:42:23 +0400

    multithreaded merge sort

--- NetBeansProjects/CppApplication_1/merge_sort.cpp
+++ NetBeansProjects/CppApplication_1/merge_sort.cpp
@@ -2,43 +2,40 @@
 #include <thread>
 #include <fstream>
 #include <string>
-#include <list>
+#include <vector>
 
 using namespace std;
 
-void merge(list<double> left, list<double> right, list<double> base) {
-  list<double>:: iterator i, j; 
-  while(i != left.end() && j != right.end()){
-    if(*i <= *j) {base.push_front(*i); i++;}
-    else {base.push_front(*j); ++j;}
+void merge(vector<double> left, vector<double> right, vector<double>& base) {
+  int i = 0, j = 0, k = 0;
+  while(i < left.size() && j < right.size()) {
+    if(left[i] <= right[j]) base[k++] = left[i++];
+    else base[k++] = right[j++];
   }
-  while( i != left.end() ) { base.push_front(*i); i++; }
-  while( j != right.end() ) { base.push_front(*j); j++; }
+  while( i < left.size() ) base[k++] = left[i++];
+  while( j < right.size() ) base[k++] = right[j++];  
 }
 
-void mergesort(bool doMultithreaded, list<double> a) {
+void mergesort(bool doMultithreaded,  vector<double>& a) {
   int n = a.size();
   if(n < 2) return;
   else {
-    unsigned int mid = abs(n/2);
-    list<double> left, right;
-    list<double>:: iterator i;
-    int n = 0;
-    for(i = a.begin(); n < a.size()/2; ++i, ++n) { left.push_front(*i);
-      for( ;i != a.end(); ++i) right.push_front(*i);
-      if( doMultithreaded == true ) {
-        thread t1( mergesort, true, left);
-        thread t2( mergesort, true, right);
-        t1.join(); 
-        t2.join(); 
-      }
-      else {
-        mergesort(false, left);
-        mergesort(false, right);
-      }
-      merge(left, right, a);
-    }    
-  }
+    vector<double> left, right;
+    int i = 0;
+    for(i = 0; i < n/2; ++i) left.push_back(a[i]);
+    for( ; i < n; ++i) right.push_back(a[i]);
+    if( doMultithreaded == true ) {
+      thread t1( mergesort, true, ref(left) );
+      thread t2( mergesort, true, ref(right) );
+      t1.join(); 
+      t2.join(); 
+    }
+    else {
+      mergesort(false, left);
+      mergesort(false, right);
+    }
+    merge(left, right, a);
+  }    
 }
 
 int main(int argc, char* argv[]) {
@@ -49,11 +46,11 @@ int main(int argc, char* argv[]) {
   else {
     fstream f;
     double value;
-    list<double> arr;  
+    vector<double> arr;  
     
     f.open(argv[1], ios::in);
     while(f >> value) {
-      arr.push_front(value);
+      arr.push_back(value);
     }
     
     cout << "\nDo you want to do sorting in multi-threaded way ? (y/n) : ";
@@ -68,8 +65,8 @@ int main(int argc, char* argv[]) {
         default : cout << "Wrong answer !!!\n"; return -1;
     }
 
-    for(list<double>:: iterator i; i != arr.end(); ++i)
-      cout << *i << " ";
+    for(int i = 0; i < arr.size(); ++i)
+      cout << arr[i] << " ";
     cout << endl;
   }
   return 0;
